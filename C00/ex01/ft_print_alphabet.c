b184e5a4fee7f0dea963355ca79542fcd417c8a3
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-10-02 19:14:18 +0200

    C00
    Arquivos do C00

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rwrobles <rwrobles@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 16:41:20 by rwrobles          #+#    #+#             */
+/*   Updated: 2024/10/02 18:11:52 by rwrobles         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		write (1, &c, 1);
+		c++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_alphabet ();
+	return (0);
+}
