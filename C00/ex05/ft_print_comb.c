60c58ed4914340b85210205678ae12e0e9509880
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-10-03 21:32:10 +0200

    Arquivos C00 - ex00 - ex05
    Faltam 3 ainda

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rwrobles <rwrobles@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/03 18:31:11 by rwrobles          #+#    #+#             */
+/*   Updated: 2024/10/03 21:00:20 by rwrobles         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	write_func(int a, int b, int c)
+{
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, &c, 1);
+	if (a != '7')
+		write(1, ", ", 2);
+	else
+		write(1, "\n", 1);
+}
+
+void	ft_print_comb(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b +1;
+			while (c <= '9')
+			{
+				write_func(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}*/
