a758dc03ef3f4f2a95ae4a47d782bb07f54a7a17
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-10-04 18:50:23 -0300

    Update rush01.c
    Update de rush01.c
    Funções linha e coluna

--- Rush00/ex00/rush01.c
+++ Rush00/ex00/rush01.c
@@ -10,4 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+void	ft_putchar(char c);
+
+void	print_line(int x, char start, char mid, char end)
+{
+	int	j;
+
+	j = 0;
+	while (j < x)
+	{
+		if (j == 0)
+			ft_putchar(start);
+		else if (j == x - 1)
+			ft_putchar(end);
+		else
+			ft_putchar(mid);
+		j++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	i = 0;
+	if (x <= 0 || y <= 0)
+		return ;
+	while (i < y)
+	{
+		if (i == 0)
+			print_line(x, '/', '*', '\\');
+		else if (i == y - 1)
+			print_line(x, '\\', '*', '/');
+		else
+			print_line(x, '*', ' ', '*');
+		i++;
+	}
+}
