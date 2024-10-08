4c2fbc39d0702dc21da6dc56d27349c0422d8878
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-10-08 15:58:30 -0300

    C01
    C01

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rwrobles <rwrobles@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 17:07:28 by rwrobles          #+#    #+#             */
+/*   Updated: 2024/10/07 19:17:24 by rwrobles         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*int	main(void)
+{
+	int	*********nbr9;
+	int	********nbr8;
+	int	*******nbr7;
+	int	******nbr6;
+	int	*****nbr5;
+	int	****nbr4;
+	int	***nbr3;
+	int	**nbr2;
+	int	*nbr1;
+	int	nbr;
+
+	nbr9 = &nbr8;
+	nbr8 = &nbr7;
+	nbr7 = &nbr6;
+	nbr6 = &nbr5;
+	nbr5 = &nbr4;
+	nbr4 = &nbr3;
+	nbr3 = &nbr2;
+	nbr2 = &nbr1;
+	nbr1 = &nbr;
+	ft_ultimate_ft(nbr9);
+	printf("%d\n", nbr);
+	return (0);
+}*/
