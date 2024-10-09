b97f1fbd530dad4b675d85f8b8b756f7c701c0a8
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-10-09 08:17:39 -0300

    Update ft_strlen.c

--- C01/ex06/ft_strlen.c
+++ C01/ex06/ft_strlen.c
@@ -11,8 +11,28 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 int	ft_strlen(char *str)
 {
-	
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
+
+// int	main(void)
+
+// {
+// 	int		len;
+// 	char	*c;
+
+// 	c = "hello world!";
+// 	len = ft_strlen(c);
+// 	printf("%d", len);
+// 	return (0);
+// }
