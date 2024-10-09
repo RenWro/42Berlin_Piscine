83913be6bf4e6401563d7b9cfb1ea2e0879558c8
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-10-09 08:18:20 -0300

    Update ft_rev_int_tab.c

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -10,4 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
 
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	temp;
+
+	i = 0;
+	while (i < size / 2)
+	{
+		temp = tab[i];
+		tab[i] = tab[size - 1 - i];
+		tab[size - 1 - i] = temp;
+		i++;
+	}
+}
+
+// int	main(void)
+// {
+// 	int	arr[5];
+
+// 	arr[0] = 1;
+// 	arr[1] = 2;
+// 	arr[2] = 3;
+// 	arr[3] = 4;
+// 	arr[4] = 5;
+// 	ft_rev_int_tab(arr, 5);
+// 	printf("%d, %d, %d, %d,%d", arr[0], arr[1], arr[2], arr[3], arr[4]);
+// 	return (0);
+// }
