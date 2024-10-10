8f43bdc46cdf6f6f268072afc2614762d54d3515
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-10-10 17:33:26 -0300

    Update ft_strlowcase.c
    EX08

--- C02/ex08/ft_strlowcase.c
+++ C02/ex08/ft_strlowcase.c
@@ -11,3 +11,33 @@
 /* ************************************************************************** */
 
 
+#include <unistd.h>
+#include <stdio.h>
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+// int	main(void)
+// {
+// 	char	str[5];
+
+// 	str[0] = 'a';
+// 	str[1] = 'b';
+// 	str[2] = 'c';
+// 	str[3] = 'd';
+// 	str[4] = '\0';
+// 	printf("%s", ft_strlowcase(str));
+// }
