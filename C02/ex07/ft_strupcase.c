58d24deb1251f8e2fb9e293254063ee12460ed12
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-10-12 08:42:24 -0300

    Atualizar o ft_strupcase.c

--- C02/ex07/ft_strupcase.c
+++ C02/ex07/ft_strupcase.c
@@ -1,42 +1,30 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strupcase.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rwrobles <rwrobles@student.42berlin.d      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/10 20:45:54 by rwrobles          #+#    #+#             */
-/*   Updated: 2024/10/10 20:45:57 by rwrobles         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 #include <stdio.h>
 
-char	*ft_strlowcase(char *str)
+char        *ft_strupcase(char *str)
 {
-	int	i;
+        int        i;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-		{
-			str[i] += 32;
-		}
-		i++;
-	}
-	return (str);
+        i = 0;
+        while (str[i] != '\0')
+        {
+                if (str[i] >= 'a' && str[i] <= 'z')
+                {
+                        str[i] -= 32;
+                }
+                i++;
+        }
+        return (str);
 }
 
-// int	main(void)
+// int        main(void)
 // {
-// 	char	str[5];
+//         char        str[5];
 
-// 	str[0] = 'a';
-// 	str[1] = 'b';
-// 	str[2] = 'c';
-// 	str[3] = 'd';
-// 	str[4] = '\0';
-// 	printf("%s", ft_strlowcase(str));
+//         str[0] = 'a';
+//         str[1] = 'b';
+//         str[2] = 'c';
+//         str[3] = 'd';
+//         str[4] = '\0';
+//         printf("%s", ft_strupcase(str));
 // }
