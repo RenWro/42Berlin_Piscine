803f179165c13f359fbc2a39b29a334a9e9790c8
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-10-16 00:23:06 +0200

    Update ft_strncmp.c

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -10,4 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && (s1[i] != '\0' && s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	if (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+		return (s1[i] - s2[i]);
+	return (0);
+}
+
+int	main(void)
+{
+	printf("%d", ft_strncmp("Hello", "42", 7));
+	printf("\n%d", ft_strncmp("Hello", "Hel", 7));
+	printf("\n%d", ft_strncmp("Hel", "Hello", 7));
+	printf("\n%d", ft_strncmp("Hello", "Hello", 7));
+}
