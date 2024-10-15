e5f79ae0b021620413089b788dc4b37872b84c2c
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-10-16 00:25:17 +0200

    Create ft_strncat.c

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,35 @@
+
+#include <unistd.h>
+#include <stdio.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb);
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0' && j < nb)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+int	main(void)
+{
+	char	src[] = "42!";
+	char	dest[50] = "Hello,";
+
+	ft_strncat(dest, src, 15);
+	printf("Concat nb result: %s\n", dest);
+	return (0);
+}
