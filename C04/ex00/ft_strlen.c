48ae14983e8d5f8245ee069632bdc9a6a9d84e03
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-10-19 10:15:42 -0300

    Update ft_strlen.c

--- C04/ex00/ft_strlen.c
+++ C04/ex00/ft_strlen.c
@@ -10,29 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <stdio.h>
 
-int        ft_strlen(char *str);
-
-int        ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-        int        i;
+	int	i;
 
-        i = 0;
-        while (str[i] != '\0')
-        {
-                i++;
-        }
-        return (i);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
-//int        main(void)
-//{
-//        char        str[] = "Hello, 42!";
-//        int                len;
+*/int	main(void)
+{
+	char	str[] = "Hello, 42!";
+	int		len;
 
-//        len = ft_strlen(str);
-//        printf("Length of the string: %d\n", len);
-//        return (0);
-//}
+	len = ft_strlen(str);
+	printf("Length of the string: %d\n", len);
+	return (0);
+}/* 
